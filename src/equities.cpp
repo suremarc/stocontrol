9da13c12ab9c6ab16a277af3061d38a3297eaac4
suremarc <8771538+suremarc@users.noreply.github.com>
2024-11-03 15:45:29 +0000

    no need to redefine in source file

--- src/equities.cpp
+++ src/equities.cpp
@@ -8,22 +8,6 @@
 
 #include "market.hpp"
 
-class Equity
-{
-public:
-    typedef const std::string ID;
-    ID ticker;
-};
-
-class TAQMarket : Market<Equity>
-{
-private:
-    std::ofstream taq_file;
-    std::unordered_map<Equity::ID, BBO> top_of_book;
-
-public:
-};
-
 namespace taq
 {
 
